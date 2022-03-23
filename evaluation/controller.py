105ab8ac3ff6017717d6f392397f4d95daa61891
Nish <nishanpantha@gmail.com>
2022-03-23 08:39:19 -0500

    Improvements to evaluation components (#3)
    * Add requiremets.txt
    
    * Bugfix template path issue in nifi_automation.py
    
    Use `__file__` to get the template path from nifi/.
    
    Also update requirements.txt
    
    * Bugfix path issue for log file in nifi_automation
    
    Use `os.path.join`
    
    * Improve log and conf path for rclone automation
    
    - Use `__file__`-based path.
    - add .gitignore for rclone directory
    
    * Make rclone params dynamic through constructor params
    
    see `RcloneAutomation.__init__(...)`
    
    * Uncomment nifi automation code  from controller

--- evaluation/controller.py
+++ evaluation/controller.py
@@ -1,34 +1,38 @@
 # read a yaml file
+import matplotlib.pyplot as plt
+import numpy as np
 import yaml
+from mft.mft_automation import MFTAutomation
 from nifi.nifi_automation import NifiAutomation
 from rclone.rclone_automation import RcloneAutomation
-from mft.mft_automation import MFTAutomation
 
-import numpy as np
-import matplotlib.pyplot as plt
 
 def generate_grapgs(title, times):
     times = times - np.min(times)
 
     for i in range(len(times)):
-        plt.barh(i + 1, times[i][1] - times[i][0], left = times[i][0])
+        plt.barh(i + 1, times[i][1] - times[i][0], left=times[i][0])
 
     plt.savefig(title + ".png")
+
 
 def caclulate_throughput(file_sizes, times):
     total_valume = 0.0
     for i in range(len(file_sizes)):
         total_valume += file_sizes[i]
-    throughput = total_valume * 8/ (np.max(times) - np.min(times))
+    throughput = total_valume * 8 / (np.max(times) - np.min(times))
     print("Throughput: " + str(throughput))
 
 
 # TODO: Fetch theses values from the S3 python client
-file_sizes = [2, 2, 2] # GB
-file_list = ["testfile2g",  "testfile2g2",  "testfile2g3"] # File list in source bucket
-nifi_installation = "/proj/MFT/nifi-1.15.3"
+file_sizes = [2, 2, 2]  # GB
+file_list = ["testfile2g", "testfile2g2", "testfile2g3"]  # File list in source bucket
+
+# nifi_installation = "/sproj/MFT/nifi-1.15.3"
+nifi_installation = "/home/nishan/software/nifi/nifi-1.15.3"
 mft_installation = "/proj/MFT/build"
-config_file = "configs.yaml"
+
+config_file = "config.yaml"
 
 ###### Nifi ###########
 automation = NifiAutomation(config_file, file_list, nifi_installation)
@@ -40,7 +44,16 @@ generate_grapgs("nifi", nifi_result)
 caclulate_throughput(file_sizes, nifi_result)
 
 ###### Rclone ###########
-automation = RcloneAutomation(config_file, file_list)
+automation = RcloneAutomation(
+    config_file,
+    file_list,
+    buffer_size=512,
+    multi_thread_streams=10,
+    multi_thread_cutoff=50,
+    ntransfers=8,
+    s3_max_upload_parts=10,
+    s3_upload_concurrency=10,
+)
 rclone_result = automation.run_automation()
 print("Rclone automation results", rclone_result)
 
