118cfe068fa09922c7b589eaabe85a972530c996
NISH1001 <nishanpantha@gmail.com>
2022-08-25 14:33:46 -0500

    Update api.py with proper base classes
    Also, restructure `tests/` directory and remove unwanted file

--- evalit/api.py
+++ evalit/api.py
@@ -1,5 +1,11 @@
-from ._base import AbstractAutomation
+# flake8: noqa
+
+from .controller import StandardAutomationController
 from .mft import MFTAutomation
 from .nifi import NifiAutomation
 from .rclone import RcloneAutomation
 from .structures import TransferDTO
+
+
+class BaseAPIs:
+    from ._base import AbstractAutomation, AbstractController
